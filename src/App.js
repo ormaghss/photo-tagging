091d7cda3749a8bcbf794872c8c899b19905bb1e
ormaghss <129315894+ormaghss@users.noreply.github.com>
2023-05-02 14:14:23 -0500

    new phot tagging with npm install

--- src/App.js
+++ src/App.js
@@ -1,60 +1,40 @@
 import React from "react";
 import ReactImageAnnotate from "react-image-annotate";
-import './App.css';
 
-function App(props) {
-  const onSave = MainLayoutState => {
+const onSave = MainLayoutState => {
 
-    console.log(JSON.stringify(MainLayoutState));
-  };
-
-  return (
-    <>
-      <div>
-        <div>
-          < ReactImageAnnotate
-            labelImages
-            regionClsList={["Benedict", "Anish", "Mathew", "Santhosh"]}
-            regionTagList={["10C", "10B", "10A"]}
-            enabledTools={["select",  "create-box"]}            
-            hideNext={true}
-            hidePrev={true}
-            showTags={true}
-            images={[
-              {
-                src: "https://res.cloudinary.com/course4u/image/upload/v1680404739/ghss/group-photo_m6pntk.jpg",
-                name: "Group Photo",
-                regions: [
-                  {
-                      "type": "box",
-                      "x": 0.882589285565218,
-                      "y": 0.11861966874654396,
-                      "w": 0.024536998956521727,
-                      "h": 0.07801313232902467,
-                      "highlighted": false,
-                      "editingLabels": false,
-                      "color": "#f44336",
-                      "cls": "Benedict",
-                      "id": "8069525039067573",
-                      "tags": [
-                          "10C"
-                      ]
-                  }
+  console.log(JSON.stringify(MainLayoutState));
+};
+const App = () => (
+  <ReactImageAnnotate
+    labelImages
+    regionClsList={["Benedict", "Anish", "Mathew", "Santhosh"]}
+    regionTagList={["10C", "10B", "10A"]}
+    images={[
+      {
+        src: "https://res.cloudinary.com/course4u/image/upload/v1680404739/ghss/group-photo_m6pntk.jpg",
+        name: "Group Photo",
+        regions:  [
+          {
+              "type": "box",
+              "x": 0.882589285565218,
+              "y": 0.11861966874654396,
+              "w": 0.024536998956521727,
+              "h": 0.07801313232902467,
+              "highlighted": false,
+              "editingLabels": false,
+              "color": "#f44336",
+              "cls": "Benedict",
+              "id": "8069525039067573",
+              "tags": [
+                  "10C"
               ]
-              }
-            ]}
-
-            onExit={onSave}
-
-            // onMouseEnter={() => setShowtag(true)}
-            // onMouseOver={() => setShowtag(true)}
-
-          />
-
-        </div>
-      </div>
-    </>
-  )
-}
+          }
+      ]
+      }
+    ]}
+    onExit={onSave}
+  />
+);
 
 export default App;
